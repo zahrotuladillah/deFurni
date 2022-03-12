d2bf8b00f1e8827f034e817173b1fdc386f09736
zahrotuladillah <72771774+zahrotuladillah@users.noreply.github.com>
2022-03-12 09:11:08 -0700

    update

---
+++ quiz1framepro/quiz1framepro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace quiz1framepro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
